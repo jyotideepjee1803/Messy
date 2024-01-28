5ec9b7eecc18fa121aab05ac4cf9e4aa9818d257
Jyotideep <jyotideepjee@gmail.com>
2024-01-28 21:09:13 +0530

    meal count

--- controller/couponController.js
+++ controller/couponController.js
@@ -1,4 +1,4 @@
-import CouponModel from "../models/coupon";
+import CouponModel from "../models/coupon.js";
 
 const couponValidity = async(req,res)=>{
     const {email, day, mealType} = req.body;
@@ -10,4 +10,28 @@ const couponValidity = async(req,res)=>{
     return false;
 }
 
-export {couponValidity};
+const totalMealCount = async(req,res)=>{
+    const allCoupons = await CouponModel.find({}); //get all purchased coupons  
+    let toMake = [
+        [0, 0, 0, 0, 0, 0, 0], // Breakfast
+        [0, 0, 0, 0, 0, 0, 0], // Lunch
+        [0, 0, 0, 0, 0, 0, 0]  // Dinner
+        ];
+    
+    allCoupons.forEach(coupon => {
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                if(coupon.week[i][j] == true) toMake[i][j]++;
+            }
+        }
+    });
+
+    const totalMeals = [];
+    for(let j=0;j<7;j++){
+        const day = {breakfast: toMake[i][0], lunch: toMake[i][1], dinner: toMake[i][2]};
+        totalMeals.push(day);
+    }
+    res.send(totalMeals);
+}
+
+export {couponValidity,totalMealCount};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { registerUser, authenticateUser } from "../controller/userController.js";
+import { couponValidity } from "../controller/couponController.js";
 
 const router = Router();
 
 router.post("/register",registerUser);
 router.post("/login",authenticateUser);
+router.post("/validCoupon",couponValidity);
 
 export default router;
