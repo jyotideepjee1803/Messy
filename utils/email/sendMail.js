b1c90dcc10a73d395259f3d776b74c5878661636
Jyotideep <jyotideepjee@gmail.com>
2024-03-13 23:58:28 +0530

    promised_mail

--- utils/email/sendMail.js
+++ utils/email/sendMail.js
@@ -4,39 +4,40 @@ import fs from "fs";
 import path from "path";
 
 const sendEmail = async(email, subject, payload, template)=>{
-    console.log( process.env.FROM_EMAIL)
-    // try{
-        const transporter = nodemailer.createTransport({
+    // console.log( process.env.FROM_EMAIL)
+    const options = {
+        from: 'jyotideepjee@gmail.com',
+        to: email,
+        subject : subject,
+        html : template,
+    };
+    try{
+        await nodemailer.createTransport({
             service: "gmail",
             auth : {
                 user : process.env.FROM_EMAIL,
                 pass : process.env.EMAIL_PASSWORD,
             }
-        })
+        }).sendMail(options)
         // console.log(transporter);
         // const source = fs.readFileSync(path.join(__dirname, template), "utf8");
         // const compiledTemplate = Handlebars.compile(source);
-        const options = {
-            from: 'jyotideepjee@gmail.com',
-            to: email,
-            subject : subject,
-            html : template,
-        };
+    
         // 
         // console.log(options);
         //email send : 
-        await new Promise((resolve, reject)=>{
-            transporter.sendMail(options, (error, info)=>{
-                if(error){
-                    // console.log(error)
-                    reject(error);
-                }
-                else{
-                    // console.log(info)
-                    resolve(info);
-                }
-            })
-        })
+        // await new Promise((resolve, reject)=>{
+        //     transporter.sendMail(options, (error, info)=>{
+        //         if(error){
+        //             // console.log(error)
+        //             reject(error);
+        //         }
+        //         else{
+        //             // console.log(info)
+        //             resolve(info);
+        //         }
+        //     })
+        // })
 
         // return res.status(200).json({
         //     success : true,
@@ -66,9 +67,9 @@ const sendEmail = async(email, subject, payload, template)=>{
         //     })
         // })
 
-    // }catch(error){
-    //     return error;
-    // }
+    }catch(error){
+        return error;
+    }
 }
 
 export default sendEmail;
